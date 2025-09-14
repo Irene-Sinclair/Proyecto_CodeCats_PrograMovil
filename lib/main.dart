6c29b86751c1a9315aa318c9c63cf65c47d29358
Cesar Flores <cesarbj11@gmail.com>
2025-09-13 18:45:30 -0600

    Cargando Firebase desde el main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'login/login.dart'; // 👈 Importa tu archivo
 import 'package:proyecto_codecats/Pantallas_Admin/panel_adm.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform
+  );
+  
   runApp(const MyApp());
 }
 
@@ -18,7 +24,8 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
       ),
-      home: const AdminSettingsScreen(), // 👈 Aquí llamamos la clase del archivo login.dart
+      home:
+          const AdminSettingsScreen(), // 👈 Aquí llamamos la clase del archivo login.dart
     );
   }
 }
